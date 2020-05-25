fixed variance error. added loss training on outliers.

model.py
@@ -11,12 +11,13 @@ def variance(y_hat, var_hat):
     sum_squares = (1/T) * (y_hat**2).sum(0)
     squared_sum = ((1/T) * y_hat.sum(0))**2
     epi_var = sum_squares - squared_sum
-    ale_var = (1/T) * (var_hat**2).sum(0)
+    ale_var = (1/T) * (var_hat).sum(0)
     return epi_var, ale_var
 
 class Model(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, dropout=0.5, device=torch.device("cpu"), num_layers=1, use_attention=False):
+    def __init__(self, input_size, hidden_size, output_size, dropout=0.5, device=torch.device("cpu"), num_layers=1):
         super(Model, self).__init__()
+
         self.device = device
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -24,7 +25,6 @@ class Model(torch.nn.Module):
         self.num_layers = num_layers
         #self.inlinear = nn.Linear(self.input_size, self.hidden_size)
         #self.relu = nn.ReLU()
-        self.use_attention = use_attention
         self.dropout = nn.Dropout(dropout)
         self.inDense = torch.nn.Sequential(
             nn.Linear(self.input_size, self.hidden_size),
@@ -32,13 +32,10 @@ class Model(torch.nn.Module):
             self.dropout
         )
 
-        if self.use_attention:
-            self.attention = Attention(self.hidden_size)
-
         self.lstm = torch.nn.LSTM(self.hidden_size,
                                   self.hidden_size,
                                   num_layers=num_layers,
-                                  dropout=dropout if num_layers > 1 else 0.0, # dropout only applied with more than one layer
+                                  dropout=0, # dropout only applied with more than one layer
                                   bidirectional=False,
                                   batch_first=True)
         #self.lstm = nn.LSTMCell(self.hidden_size, self.hidden_size) #nn.LSTMCell
@@ -96,12 +93,6 @@ class Model(torch.nn.Module):
         output, (h_t, c_t) = self.lstm(input, (h_t, c_t))
         lstm_outputs = self.dropout(output)
 
-        if self.use_attention:
-            query = lstm_outputs
-            context = lstm_outputs
-            values = lstm_outputs
-            lstm_outputs, weights = self.attention(query, context, values)
-
         output = self.outlinear(lstm_outputs)
 
         outputs = output[:, :, 0, None]
@@ -127,13 +118,6 @@ class Model(torch.nn.Module):
 
             future_input, (h_t, c_t) = self.lstm(future_input[:, None, :], (h_t, c_t))
             future_input = self.dropout(future_input)
-
-            if self.use_attention:
-                #forecasted_outputs = torch.stack(future_outputs, 1)
-                #all_outputs = torch.cat([outputs, forecasted_outputs], 1)
-                context = torch.cat([context,future_input],1)
-                query = future_input
-                future_input, weights = self.attention(query, context, context)
 
             future_input_logvariance = self.outlinear(future_input)
 

train.py
@@ -32,8 +32,8 @@ def main():
                   hidden_size=hidden_size,
                   num_layers=num_layers,
                   output_size=1,
-                  device=device,
-                  use_attention=use_attention)
+                  dropout=0.5,
+                  device=device)
 
     #model.load_state_dict(torch.load("/tmp/model_epoch_0.pth")["model"])
     model.train()
@@ -81,14 +81,20 @@ def main():
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-4)
 
+    cutoff_after_n_epochs=2
+    loss_thresshold = -1
+
     stats=list()
     for epoch in range(epochs):
 
-        trainloss = train_epoch(model,dataloader,optimizer, criterion, device)
+        trainloss = train_epoch(model,dataloader,optimizer, criterion, device, loss_thresshold)
         testmetrics, testloss = test_epoch(model,validdataloader,device, criterion, n_predictions=1)
         metric_msg = ", ".join([f"{name}={metric.compute():.2f}" for name, metric in testmetrics.items()])
-        msg = f"epoch {epoch}: train loss {trainloss:.2f}, test loss {testloss:.2f}, {metric_msg}"
+        msg = f"epoch {epoch}: train loss {trainloss.mean():.2f}, test loss {testloss:.2f}, {metric_msg}"
         print(msg)
+
+        idx = trainloss.sort()[1][int(len(trainloss) * 0.6)]
+        loss_thresshold = trainloss[idx]
 
         test_model(model, validdataset, device)
 
@@ -108,7 +114,8 @@ def main():
 
     df = pd.DataFrame(stats)
 
-def train_epoch(model, dataloader, optimizer, criterion, device):
+def train_epoch(model, dataloader, optimizer, criterion, device, loss_threshold=-1):
+    model.dropout.eval()
     iterator = tqdm(enumerate(dataloader), total=len(dataloader))
     losses = list()
     for idx, batch in iterator:
@@ -127,15 +134,18 @@ def train_epoch(model, dataloader, optimizer, criterion, device):
         # y is used for teacher forcing during training
         y_pred, log_variances = model(x_data, y=y_true, date=doy)
         loss = criterion(y_pred, y_true, log_variances)
-        losses.append(loss)
-        loss.backward()
-        optimizer.step()
+
+        if loss < loss_threshold:
+            loss.backward()
+            optimizer.step()
+        losses.append(loss.detach())
         iterator.set_description(f"loss {loss:.4f}, mean log_variances {log_variances.mean():.6f}")
 
-    return torch.stack(losses).mean()
+    return torch.stack(losses)
 
 def test_epoch(model,dataloader, device, criterion, n_predictions):
-
+    # switch on dropout
+    model.dropout.train()
     metrics = dict(
         mae=ignite.metrics.MeanAbsoluteError(),
         mse=ignite.metrics.MeanSquaredError(),
@@ -180,12 +190,12 @@ def test_model(model, dataset, device):
         x = dataset.data[idx].astype(float)
         date = dataset.date[idx].astype(np.datetime64)
         N_seen_points = 250
-        N_predictions = 10
+        N_predictions = 50
 
         make_and_plot_predictions(model, x, date, N_seen_points=N_seen_points, N_predictions=N_predictions, device=device,meanstd=(dataset.mean,dataset.std))
         plt.show()
 
-        idx = 20
+        idx = 17
 
         x = dataset.data[idx].astype(float)
         date = dataset.date[idx].astype(np.datetime64)